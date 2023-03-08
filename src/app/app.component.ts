3818b42ab74ce7ee7f1997f6b4b19a72fe788363
topdev-909 <topdev909@gmail.com>
2023-03-08 06:51:09 -0800

    Updated date format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,38 @@ import {
 import { MatDialog } from '@angular/material/dialog';
 import { ResultDialogComponent } from './result-dialog/result-dialog.component';
 import * as dayjs from 'dayjs';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { DatePipe } from '@angular/common';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+const APP_FORMATS = {
+  parse: {
+    dateInput: "DD/MM/YYYY"
+  },
+  display: {
+    dateInput: "DD/MM/YYYY",
+    monthYearLabel: "MMM YYYY",
+    dateA11yLabel: "DD/MM/YYYY",
+    monthYearA11yLabel: "MMMM YYYY"
+  }
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE]
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: APP_FORMATS
+    },
+    DatePipe
+  ],
 })
 export class AppComponent {
   timeList: any = [];
